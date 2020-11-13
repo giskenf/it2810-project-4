237fa8588b9b6e8a5b6d0ebeb0dd0ed95e7212b6
giskenf <giskefreberg@hotmail.com>
2020-11-13 17:36:15 +0100

    Add search-component #10

--- my-new-project/screens/TabOneScreen.tsx
+++ my-new-project/screens/TabOneScreen.tsx
@@ -21,47 +21,26 @@ import { Text, View } from '../components/Themed';
 import {playerAbility} from "../store/types";
 import {Player} from "../components/PlayerInfo";
 
-
 export default function TabOneScreen() {
     const dispatch = useDispatch();
     const [playerName, setPlayerName] = useState("");
     const playerState = useSelector((state: RootStore) => state.players);
-
-    //const [people,setPeople] = useState([] as any[]);
-
-    const [people,setPeople] = useState([
-        {_id:"1", yellow_cards:0,red_cards:0,team:"Arsenal",own_goals:0,assists: 0, clean_sheets: 0, goals_conceded: 0, goals_scored: 1, name: "Evrin Özil", news: "Not included in Arsenal's 25-man"},
-        {_id:"2", yellow_cards:0,red_cards:0,team:"Arsenal",own_goals:0,assists: 0, clean_sheets: 0, goals_conceded: 0, goals_scored: 2, name: "Grof Özil", news: "Not included in Arsenal's 25-man"},
-        {_id:"3", yellow_cards:0,red_cards:0,team:"Chelsea",own_goals:0,assists: 10, clean_sheets: 0, goals_conceded: 0, goals_scored: 5, name: "Mesut Özil", news: "Not included in Arsenal"},
-        {_id:"4", yellow_cards:0,red_cards:0,team:"Man Utd",own_goals:0,assists: 0, clean_sheets: 0, goals_conceded: 5, goals_scored: 0, name: "Fjr Özil", news: "Not"},
-        {_id:"5", yellow_cards:1,red_cards:1,team:"Arsenal",own_goals:0,assists: 0, clean_sheets: 0, goals_conceded: 6, goals_scored: 0, name: "Mesut Özil Mesut Özil", news: "Not included in Arsenal's 25-manNot included in Arsenal's 25-man"},
-        {_id:"6", yellow_cards:0,red_cards:3,team:"Arsenal",own_goals:0,assists: 0, clean_sheets: 0, goals_conceded: 7, goals_scored: 0, name: "Mesut Mesut Özil", news: "Not included in Arsenal's 25-manNot included in Arsenal's 25-manNot included in Arsenal's 25-man"},
-        {_id:"7", yellow_cards:0,red_cards:4,team:"Arsenal",own_goals:0,assists: 0, clean_sheets: 0, goals_conceded: 0, goals_scored: 1, name: "Evrin Özil", news: "Not included in Arsenal's 25-man"},
-        {_id:"8", yellow_cards:0,red_cards:0,team:"Arsenal",own_goals:0,assists: 0, clean_sheets: 0, goals_conceded: 0, goals_scored: 2, name: "Grof Özil", news: "Not included in Arsenal's 25-man"},
-        {_id:"9", yellow_cards:0,red_cards:0,team:"Arsenal",own_goals:0,assists: 10, clean_sheets: 0, goals_conceded: 0, goals_scored: 5, name: "Mesut Özil", news: "Not included in Arsenal"},
-        {_id:"10", yellow_cards:5,red_cards:1,team:"Arsenal",own_goals:0,assists: 0, clean_sheets: 0, goals_conceded: 5, goals_scored: 0, name: "Fjr Özil", news: "Not"},
-        {_id:"11", yellow_cards:1,red_cards:0,team:"Arsenal",own_goals:0,assists: 0, clean_sheets: 0, goals_conceded: 6, goals_scored: 0, name: "Mesut Özil Mesut Özil", news: "Not included in Arsenal's 25-manNot included in Arsenal's 25-man"},
-        {_id:"12", yellow_cards:0,red_cards:0,team:"Arsenal",own_goals:0,assists: 10, clean_sheets: 0, goals_conceded: 0, goals_scored: 5, name: "Mesut Özil", news: "Not included in Arsenal"},
-        {_id:"13", yellow_cards:0,red_cards:0,team:"Arsenal",own_goals:0,assists: 0, clean_sheets: 0, goals_conceded: 5, goals_scored: 0, name: "Fjr Özil", news: "Not"},
-        {_id:"14", yellow_cards:0,red_cards:0,team:"Arsenal",own_goals:0,assists: 0, clean_sheets: 0, goals_conceded: 6, goals_scored: 0, name: "Mesut Özil Mesut Özil", news: "Not included in Arsenal's 25-manNot included in Arsenal's 25-man"},
-        {_id:"15", yellow_cards:0,red_cards:4,team:"Arsenal",own_goals:0,assists: 0, clean_sheets: 0, goals_conceded: 7, goals_scored: 0, name: "Mesut Mesut Özil", news: "Not included in Arsenal's 25-manNot included in Arsenal's 25-manNot included in Arsenal's 25-man"}])
-
-
-
+    const [value, onChangeText] = React.useState('Useless Placeholder');
 
     useEffect(()=>{
         dispatch(GetPlayers(playerName, "", "", 1, 1))
-    },[])
+    },[playerName])
 
-    const [visible, setVisible] = useState(false);
-
-    const toggleOverlay = () => {
-        setVisible(!visible);
-    };
-    console.log(playerState.player)
+    //console.log(playerState.player)
+    console.log(playerName);
     return (
 
         <View style={styles.container}>
+            <TextInput
+              style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+              onChangeText={text => setPlayerName(text)}
+              value={playerName}
+            />
             <FlatList
                 data={playerState.player}
                 keyExtractor={(item)=> item._id}
